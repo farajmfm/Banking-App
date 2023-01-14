0cbecd4d606d2e6a4247b3a6d1bf941e9ab72226
mohomed faraj <86347430+farajmfm@users.noreply.github.com>
2023-01-14 18:09:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'bulma'
+
+function App() {
+  return (
+    <div className='section'>
+      <h1 className='title has-text-centered is-size-1'>Banking App</h1>
+      <hr /> 
+    </div>
+  );
+}
+
+export default App;
